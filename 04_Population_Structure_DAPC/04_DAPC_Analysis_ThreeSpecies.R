c5cbc95ed67c08feb9c86b82a9e6d58165e349f4
Sam Donohoo <sad0046@auburn.edu>
2025-04-18 14:43:30 -0500

    Modified R Markdown

--- 04_Population_Structure_DAPC/04_DAPC_Analysis_ThreeSpecies.R
+++ 04_Population_Structure_DAPC/04_DAPC_Analysis_ThreeSpecies.R
@@ -5,38 +5,27 @@ library(poppr)
 library(na.tools)
 library(paletteer)
 
-# Load data from Multiple SNP Genepop File produced by Stacks
-data <- read.genepop(file="04_Population_Structure_DAPC/Lithasia_Duck_min_max_NBA_NoOut_DAPC3.snps.vcf")
+# Load Lithasia.data from Multiple SNP Genepop File produced by Stacks
+Lithasia.data <- read.genepop(file="04_Population_Structure_DAPC/Lithasia_Duck_min_max_NBA_NoOut_DAPC3.snps.gen")
 
-# Find the # of Principle Components
-grp <- find.clusters(data, max.n.clust=30)
+# Find the Best-Fit # of Principle Components (Genetic Clusters)
+grp <- find.clusters(Lithasia.data, max.n.clust=30, n.pca = 500, n.clust = 7)
 
-# Run DAPC Using the Best-Fit # of Clusters
-dapc2 <- dapc(data,grp$grp)
+# Run DAPC Using the Best-Fit # of Clusters, but Apply (NAME) K-1
+dapc2 <- dapc(Lithasia.data,grp$grp, n.pca = 6, n.da = 5)
 
 # Create a Color-Blind Friendly Palette
 cbbPalette <- c("#56B4E9","#E69F00", "#0072B2", "#009E73", "#F0E442", "#D55E00", "#CC79A7", "gray")
 
 # Graph the DAPC Results. Uses Built in Graphing Functions in the DAPC Package.
-scatter(dapc2, scree.da=FALSE, bg="white", pch=20, cell=0, 
-        cstar=0, col=cbbPalette, solid=.4, cex=3,clab=0, leg=TRUE)
+# Initial Scatterplot with Cluster Assignments
+scatter(dapc2, scree.da=FALSE, bg="white", pch=20, cell=0,
+        cstar=0, col=cbbPalette, solid=.4, cex=3,clab=0, leg=TRUE, posi.leg = "bottomright")
 
-scatter.dapc(dapc2, ratio.pca=0.3,solid=0.4,cex=3,clab=0,cell=0,posi.da="topleft",
-             bg="white",pch=20, cstar=0, col=cbbPalette, scree.pca=TRUE, 
-             posi.pca="topright", legend = TRUE, posi.leg = "top",
-             label.inds = list(air = 0.4, pch = 20))
-
-scatter.dapc(dapc2, ratio.pca=0.3,solid=0.4,cex=3,clab=0,cell=0,posi.da="topleft",
-             bg="white",pch=20, cstar=0, col=cbbPalette, scree.pca=TRUE, 
-             posi.pca="topright", legend = FALSE, posi.leg = "top",
-             label.inds = list(air = 0.4, pch = 20))
-
-# Dots w Group Label
-scatter(dapc2, ratio.pca=0.3,solid=0.4,cex=3,posi.da="topleft",
-        bg="white",pch=20, cstar=0, col=cbbPalette, scree.pca=TRUE, posi.pca="topright",
-        label.inds = TRUE, legend = TRUE, posi.leg = "right")
-
-scatter(dapc2, ratio.pca=0.3,solid=0.4,cex=3, scree.da=FALSE,
+# Final Scatterplot with Cluster Assignments, and DA / PCA Eigenvalues
+scatter(dapc2, solid=0.4, cex=3, 
+        ratio.da = 0.2, posi.da = "topright", scree.da = TRUE,
+        posi.pca = "bottomright", scree.pca = TRUE, ratio.pca=0.3,
         bg="white",pch=20, cstar=0, col=cbbPalette,
         label.inds = TRUE)
 
@@ -67,6 +56,6 @@ assignplot(dapc2)
 
 assignplot(dapc2, subset = 201:250)
 
-write.csv(grp[["grp"]], "DuckRiver_DAPC_k6.csv")
+write.csv(grp[["grp"]], "04_Population_Structure_DAPC/DuckRiver_DAPC_k7.csv")
 
 dev.off()
